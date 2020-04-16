a9897732437e9575f67660c36637d469b94f312c
cheebou28 <cheebou28@gmail.com>
2020-04-16 14:31:07 +0900

    QRcodeScreen Styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions';
 
@@ -28,7 +28,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Scan</Text>
       <Text style={styles.description}>RECUPのQRコードをスキャンしてください</Text>
+      <Image
+        source={{ uri: 'http://drive.google.com/uc?export=view&id=1kU2L0kwWvmxfxg5n9PAemGSastsb_53E' }}
+        style={{ alignSelf: 'center', width:120, height:120, backgroundColor: '#fff' }}
+      />
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={styles.barcode}
@@ -43,20 +48,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  title: {
+    marginTop: '10%',
+    alignSelf: 'center',
+    fontSize: 26,
+  },
   barcode: {
     flex: 1,
     height: '55%',
     marginLeft:40,
     marginRight:40,
+    marginTop: '3%',
     borderRadius: 20,
+    ...Platform.select({
+      ios: {
+        borderRadius: 20,
+      },
+    }),
   },
   description: {
     fontSize: 16,
-    marginTop: '40%',
+    marginTop: '5%',
     alignSelf: 'center',
+    textDecorationLine: 'underline',
+    color: '#A8A3A3',
   },
   goback: {
+    marginTop: '10%',
     marginBottom: '15%',
     alignSelf: 'center',
+    textDecorationLine: 'underline',
+    color: '#A8A3A3',
+    fontSize: 16,
   },
 });
--- src/screens/QRcodeScreen.js
+++ src/screens/QRcodeScreen.js
@@ -1,34 +1,84 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, Linking } from 'react-native';
-import QRCodeScanner from 'react-native-qrcode-scanner';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
 
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <QRCodeScanner
-        ref={(node) => { this.scanner = node; }}
-        onRead={this.onSuccess.bind(this)}
-      />
-    );
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
   }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Scan</Text>
+      <Text style={styles.description}>RECUPのQRコードをスキャンしてください</Text>
+      <Image
+        source={{ uri: 'http://drive.google.com/uc?export=view&id=1kU2L0kwWvmxfxg5n9PAemGSastsb_53E' }}
+        style={{ alignSelf: 'center', width:120, height:120, backgroundColor: '#fff' }}
+      />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={styles.barcode}
+      />
+      <Text style={styles.goback}>Go Back</Text>
+      {scanned && <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  centerText: {
+  container: {
     flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
   },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
+  title: {
+    marginTop: '10%',
+    alignSelf: 'center',
+    fontSize: 26,
   },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
+  barcode: {
+    flex: 1,
+    height: '55%',
+    marginLeft:40,
+    marginRight:40,
+    marginTop: '3%',
+    borderRadius: 20,
+    ...Platform.select({
+      ios: {
+        borderRadius: 20,
+      },
+    }),
   },
-  buttonTouchable: {
-    padding: 16,
+  description: {
+    fontSize: 16,
+    marginTop: '5%',
+    alignSelf: 'center',
+    textDecorationLine: 'underline',
+    color: '#A8A3A3',
+  },
+  goback: {
+    marginTop: '10%',
+    marginBottom: '15%',
+    alignSelf: 'center',
+    textDecorationLine: 'underline',
+    color: '#A8A3A3',
+    fontSize: 16,
   },
 });
